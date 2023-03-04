de93f81768aebf29f6dc02601d0315ef6399619b
darolp <dariopizzi12@gmail.com>
2023-03-04 10:16:38 -0300

    cambios

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+  Textarea,
+  Input
+} from '@chakra-ui/react'
 
 function Contact() {
+  const [contactInfo, setContactInfo] = useState({
+    name: '',
+    email: '',
+    message: ''
+  })
+
+  const handleInputChange = (e) => setContactInfo({...contactInfo, [e.target.name]: e.target.value})
+
+
   return (
-    <div>
-      
+    <div className='contactContainer'>
+      <div className='contactForm'>
+        <FormControl isRequired>
+          <FormLabel>nombre</FormLabel>
+          <Input type='text' value={contactInfo.name} name="name" onChange={handleInputChange} />
+        </FormControl>
+        <FormControl isRequired>
+          <FormLabel>Correo</FormLabel>
+          <Input type='email' value={contactInfo.email} name="email" onChange={handleInputChange} />
+        </FormControl>
+        <FormControl isRequired>
+          <FormLabel>Mensaje</FormLabel>
+          <Textarea placeholder='Escriba su mensaje aqui' value={contactInfo.message} name='message' onChange={handleInputChange}/>
+        </FormControl>
+      </div>
     </div>
   )
 }
